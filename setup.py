29be0ab7b13c04515f3553428e3cd74b351049c1
Sakiko Sampei <ss372@itl202.student.eecs.qmul.ac.uk>
2017-03-21 13:39:16 +0000

    req

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='YourAppName',
       author='Your Name',
       author_email='example@example.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask','unirest','flask-wtf','wtfforms'],
+      install_requires=['Flask','unirest','flask-wtf','wtfforms','NumPy','Six','Jinja2','Requests','Tornado >4.0','PyYaml','DateUtil','Bokeh==0.12.4','Futures','Flask-PyMongo']
       )
